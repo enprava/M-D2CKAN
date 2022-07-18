732bcfa81cb708554bc5d1d550c70050f6dbe53a
enprava <epradavazquez@gmail.com>
2022-07-18 13:14:20 +0200

    Fichero consulta.py reformateado

--- src/mdm/consulta.py
+++ src/mdm/consulta.py
@@ -1,15 +1,7 @@
-import requests
-
 
 class Consulta:
     """Clase encargada de hacer una petición a la api del M&D Manager para obtener los Dataflows indicados
     en el fichero configuracion/consultas.yaml. Si no hay ninguno especificado obtendrá todos."""
 
     def __init__(self):
-            pass
-
-
-
-
-
-
+        pass
